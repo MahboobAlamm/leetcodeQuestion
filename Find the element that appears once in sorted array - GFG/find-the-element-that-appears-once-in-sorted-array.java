c0f8c35acdc925157d6838d26249d081d6cf8f6c
Mahboob Alam <76192979+MahboobAlamm@users.noreply.github.com>
2023-04-30 22:22:32 +0530

    Added solution - LeetHub

---
+++ Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0;i < n;i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            System.out.println(sol.findOnce(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution 
+{
+    int findOnce(int arr[], int n)
+    {
+        if(n<2){
+            return arr[0];
+        }
+        
+        int x = arr[0]^arr[1];
+        for(int i=2;i<n;i++){
+            x = x ^ arr[i];
+        }
+        
+        return x;
+    }
+}
