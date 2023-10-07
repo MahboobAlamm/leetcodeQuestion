48ff8dc2d20b9509ef5cf55c27bbbf05f3c4f960
Mahboob Alam <76192979+MahboobAlamm@users.noreply.github.com>
2023-10-07 14:07:24 +0530

    Added solution - LeetHub

---
+++ N meetings in one room - GFG/n-meetings-in-one-room.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++)
+                start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) 
+                end[i] = Integer.parseInt(inputLine[i]);
+                
+            int ans = new Solution().maxMeetings(start, end, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        int intervals[][] = new int[start.length][2];
+        for(int i=0;i<intervals.length;i++){
+            intervals[i][0] = start[i];
+            intervals[i][1] = end[i];
+        }
+        
+        Arrays.sort(intervals, (a,b) -> (a[1]-b[1]));
+        
+        // for(int x[]: intervals){
+        //     for(int y: x){
+        //         System.out.print(y+" ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        int curr = intervals[0][1];
+        int count = 1;
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0] > curr){
+                count++;
+                curr = intervals[i][1];
+            } else{
+                continue;
+            }
+        }
+        
+        return count;
+    }
+}
+  
