214c2cf5ae39627d0e8fe76a02494561e97c0435
Mahboob Alam <76192979+MahboobAlamm@users.noreply.github.com>
2023-10-13 22:12:36 +0530

    Added solution - LeetHub

---
+++ Repeated Character - GFG/repeated-character.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s;
+            s = sc.next();
+            
+            Solution ob = new Solution();
+            char res = ob.firstRep(s);
+            if (res == '#')
+                System.out.println(-1);
+            else
+                System.out.println(res);
+             
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    char firstRep(String s)
+    {
+        
+        for(int i=0;i<s.length()-1;i++){
+            String str = s.substring(i+1);
+            if(str.contains(""+s.charAt(i)))
+                return s.charAt(i);
+        }
+        
+        return '#';
+    }
+}
