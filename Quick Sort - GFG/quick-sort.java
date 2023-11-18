3425ab654e9324c131d4201280e0cafd53ede021
Mahboob Alam <76192979+MahboobAlamm@users.noreply.github.com>
2023-11-18 10:45:21 +0530

    Added solution - LeetHub

--- Quick Sort - GFG/quick-sort.java
+++ Quick Sort - GFG/quick-sort.java
@@ -40,30 +40,29 @@ class Solution
         if(low < high){
             
             int p = partition(arr, low, high);
-            quickSort(arr, low, p);
+            
+            quickSort(arr, low, p-1);
             quickSort(arr, p+1, high);
-        }
+        }   
     }
     static int partition(int arr[], int low, int high)
     {
         // your code here
-        int pivot = arr[low];
+        int pivot = arr[high];
+        
         int i = low-1;
-        int j = high + 1;
-        while(true){
-            do{
+        for(int j = low;j<=high-1;j++){
+            if(arr[j] < pivot){
                 i++;
-            }while(arr[i] < pivot);
-            do{
-                j--;
-            }while(arr[j] > pivot);
-            
-            if(i>= j)   
-                return j;
-                
-            int temp =  arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        
+        return i+1;
     } 
 }
